7ae3f60231a2fa7a2055550f4d7438612a90a885
Mike Bronner <mike@genealabs.com>
2014-11-05 19:56:12 -0800

    standardize the add new buttons

--- src/views/entities/index.blade.php
+++ src/views/entities/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('innerContent')
     <div class="page-header">
-        {{ link_to_route('entities.create', 'Add New Permission', null, ['class' => 'btn btn-default pull-right']) }}
+        {{ link_to_route('entities.create', 'Add New Entity', null, ['class' => 'btn btn-success btn-lg pull-right']) }}
         <h1>Entities</h1>
     </div>
     <div class="list-group">
