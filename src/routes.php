c2bb4acb41a85ec264e7b4bcdecbd1d4a63f4a7e
Mike Bronner <mike@genealabs.com>
2014-11-07 19:09:32 -0800

    added default error view and controller

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,8 @@
 <?php
-use \Illuminate\Support\Facades\Route;
 
-Route::resource('roles', 'GeneaLabs\Bones\Keeper\Controllers\RolesController');
-Route::resource('entities', 'GeneaLabs\Bones\Keeper\Controllers\EntitiesController');
-Route::resource('userroles', 'GeneaLabs\Bones\Keeper\Controllers\UserRolesController');
+use Illuminate\Support\Facades\Route;
+
+Route::resource('roles', 'GeneaLabs\Bones\Keeper\Controllers\RolesController', ['except' => ['show']]);
+Route::resource('entities', 'GeneaLabs\Bones\Keeper\Controllers\EntitiesController', ['except' => ['show']]);
+Route::resource('userroles', 'GeneaLabs\Bones\Keeper\Controllers\UserRolesController', ['only' => ['index', 'store']]);
+Route::get('invalid', ['as' => 'invalid', 'uses' => 'GeneaLabs\Bones\Keeper\Controllers\ErrorsController@invalid']);
