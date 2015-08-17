906de22b5f6b6ba8a64e444ad425d4faca7c2b62
Mike Bronner <mike@genealabs.com>
2015-08-16 17:03:54 -0700

    Fix public asset location references

--- src/GeneaLabs/Bones/Keeper/BonesKeeperServiceProvider.php
+++ src/GeneaLabs/Bones/Keeper/BonesKeeperServiceProvider.php
@@ -23,7 +23,7 @@ class BonesKeeperServiceProvider extends ServiceProvider
             require __DIR__ . '/../../../routes.php';
         }
 
-        $this->publishes([__DIR__ . '/../../../config/config.php' => config_path('/genealabs/bones-keeper.php')], 'genealabs-bones-keeper');
+        $this->publishes([__DIR__ . '/../../../config/config.php' => config_path('/genealabs-bones-keeper.php')], 'genealabs-bones-keeper');
         $this->publishes([__DIR__ . '/../../../../public' => public_path('genealabs/bones-keeper')], 'genealabs-bones-keeper');
         $this->loadViewsFrom(__DIR__.'/../../../views', 'genealabs-bones-keeper');
     }
--- src/views/master.blade.php
+++ src/views/master.blade.php
@@ -1,11 +1,11 @@
 @extends(config('genealabs-bones-keeper.layoutView'))
 
 @section('content')
-    <script src="{!! asset('genealabs-bones-keeper/js/scripts.js') !!}"></script>
-    <script src="{!! asset('genealabs-bones-keeper/js/selectize.min.js') !!}"></script>
+    <script src="{!! asset('genealabs/bones-keeper/js/scripts.js') !!}"></script>
+    <script src="{!! asset('genealabs/bones-keeper/js/selectize.min.js') !!}"></script>
     <style>
-        @import url('{!! asset('/genealabs/bones-keeper/css/selectize.bootstrap3.css') !!}');
-        @import url('{!! asset('/genealabs/bones-keeper/css/styles.css') !!}');
+        @import url('{!! asset('genealabs/bones-keeper/css/selectize.bootstrap3.css') !!}');
+        @import url('{!! asset('genealabs/bones-keeper/css/styles.css') !!}');
     </style>
     <div class="container">
         <div class="col-sm-2">
