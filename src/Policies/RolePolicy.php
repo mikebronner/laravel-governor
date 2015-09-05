c414a2223db1bd3e0acf60610e035264e7f6b3fe
Mike Bronner <mike@genealabs.com>
2015-09-04 17:18:49 -0700

    Fix incorrect entity assignment in Role policy

--- src/Policies/RolePolicy.php
+++ src/Policies/RolePolicy.php
@@ -6,26 +6,26 @@ class RolePolicy extends LaravelGovernorPolicy
 {
     public function create($user, Role $role)
     {
-        return $this->validatePermissions($user, 'create', 'announcement', $role->created_by);
+        return $this->validatePermissions($user, 'create', 'role', $role->created_by);
     }
 
     public function edit($user, Role $role)
     {
-        return $this->validatePermissions($user, 'edit', 'announcement', $role->created_by);
+        return $this->validatePermissions($user, 'edit', 'role', $role->created_by);
     }
 
     public function view($user, Role $role)
     {
-        return $this->validatePermissions($user, 'view', 'announcement', $role->created_by);
+        return $this->validatePermissions($user, 'view', 'role', $role->created_by);
     }
 
     public function inspect($user, Role $role)
     {
-        return $this->validatePermissions($user, 'inspect', 'announcement', $role->created_by);
+        return $this->validatePermissions($user, 'inspect', 'role', $role->created_by);
     }
 
     public function remove($user, Role $role)
     {
-        return $this->validatePermissions($user, 'remove', 'announcement', $role->created_by);
+        return $this->validatePermissions($user, 'remove', 'role', $role->created_by);
     }
 }
