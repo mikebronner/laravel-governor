4f421436f15fe192700ab8689fcdbd58ba56c3bd
Mike Bronner <mike@genealabs.com>
2016-01-24 17:50:13 -0800

    Set incrementing property to false

--- src/Entity.php
+++ src/Entity.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entity extends Model
 {
+    public $incrementing = false;
+
     protected $primaryKey = 'name';
     protected $rules = [
         'name' => 'required|min:3|unique:entities,name',
--- src/Ownership.php
+++ src/Ownership.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ownership extends Model
 {
+    public $incrementing = false;
+
     protected $primaryKey = 'name';
     protected $rules = [
         'name' => 'required|min:3',
