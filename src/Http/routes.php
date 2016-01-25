22c1ef164e5584ab1e4abc21d2222011eb7fc750
Mike Bronner <mike@genealabs.com>
2016-01-24 17:50:26 -0800

    Add routes to web route group

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,5 +4,8 @@ use GeneaLabs\LaravelGovernor\Http\Controllers\AssignmentsController;
 use GeneaLabs\LaravelGovernor\Http\Controllers\RolesController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('genealabs/laravel-governor/roles', RolesController::class, ['except' => ['show']]);
-Route::resource('genealabs/laravel-governor/assignments', AssignmentsController::class, ['only' => ['index', 'store']]);
+Route::group(['middleware' => ['web']], function () {
+    Route::resource('genealabs/laravel-governor/roles', RolesController::class, ['except' => ['show']]);
+    Route::resource('genealabs/laravel-governor/assignments', AssignmentsController::class,
+        ['only' => ['index', 'store']]);
+});
