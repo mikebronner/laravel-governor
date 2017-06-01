0bb4a02c48ea4550f2936d4f4af685136cbb92e0
Mike Bronner <mike@genealabs.com>
2017-06-01 16:51:49 -0700

    Updated routes to use dedicated folder

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use \GeneaLabs\LaravelGovernor\Http\Controllers\AssignmentsController;
+use \GeneaLabs\LaravelGovernor\Http\Controllers\RolesController;
+use \Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => ['web'], 'as' => 'genealabs.laravel-governor.'], function () {
+    Route::resource('genealabs/laravel-governor/roles', RolesController::class, ['except' => ['show']]);
+    Route::resource('genealabs/laravel-governor/assignments', AssignmentsController::class, ['only' => ['index', 'store']]);
+});
