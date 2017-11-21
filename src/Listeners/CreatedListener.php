ecb511c44692202b396b072d0e129009ca31f768
Mike Bronner <mike@genealabs.com>
2017-11-20 19:40:40 -0800

    Handle edge-cases when database is not fully populated

--- src/Listeners/CreatedListener.php
+++ src/Listeners/CreatedListener.php
@@ -1,17 +1,20 @@
 <?php namespace GeneaLabs\LaravelGovernor\Listeners;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class CreatedListener
 {
     /**
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @SuppressWarnings(PHPMD.StaticAccess)
      */
     public function handle(string $event, array $models)
     {
-        foreach ($models as $model) {
-            if (get_class($model) === config('genealabs-laravel-governor.auth-model')) {
-                $model->roles()->attach('Member');
+        if (Schema::hasTable('roles')) {
+            foreach ($models as $model) {
+                if (get_class($model) === config('genealabs-laravel-governor.auth-model')) {
+                    $model->roles()->attach('Member');
+                }
             }
         }
     }
--- src/Providers/LaravelGovernorService.php
+++ src/Providers/LaravelGovernorService.php
@@ -56,6 +56,7 @@ class LaravelGovernorService extends AggregateServiceProvider
             __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/genealabs/laravel-governor')
         ], 'views');
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'genealabs-laravel-governor');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
         if (Schema::hasTable('entities')) {
             $this->parsePolicies($gate);
