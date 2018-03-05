9d0b11d6f99d3c346a851b707e959ea53bfb5cce
Mike Bronner <mike@genealabs.com>
2018-03-05 08:38:44 -0800

    WIP

--- src/Listeners/CreatingListener.php
+++ src/Listeners/CreatingListener.php
@@ -1,5 +1,6 @@
 <?php namespace GeneaLabs\LaravelGovernor\Listeners;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -11,17 +12,24 @@ class CreatingListener
      */
     public function handle(string $event, array $models)
     {
-        foreach ($models as $model) {
-            if (auth()->check() && ! (property_exists($model, 'isGoverned') && $model['isGoverned'] === false)) {
-                $model->governor_created_by = auth()->user()->getKey();
-                $table = $model->getTable();
+        collect($models)
+            ->filter(function ($model) {
+                return $model instanceof Model;
+            })
+            ->each(function ($model) {
+                if (auth()->check()
+                    && ! (property_exists($model, 'isGoverned')
+                        && $model['isGoverned'] === false)
+                ) {
+                    $model->governor_created_by = auth()->user()->getKey();
+                    $table = $model->getTable();
 
-                if (! Schema::hasColumn($table, 'governor_created_by')) {
-                    Schema::table($table, function (Blueprint $table) {
-                        $table->integer('governor_created_by')->unsigned()->nullable();
-                    });
+                    if (! Schema::hasColumn($table, 'governor_created_by')) {
+                        Schema::table($table, function (Blueprint $table) {
+                            $table->integer('governor_created_by')->unsigned()->nullable();
+                        });
+                    }
                 }
-            }
-        }
+            });
     }
 }
