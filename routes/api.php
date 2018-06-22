8ccbed33c6bdaec760deac9374e8475bfa0b635b
Mike Bronner <mike@genealabs.com>
2018-06-22 15:05:13 -0700

    Add route for user-is api check

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,5 @@
 
 Route::get('user-can/{ability}', "UserCan@show")
     ->name("genealabs.laravel-governor.api.user-can.show");
+Route::get('user-is/{role}', "UserIs@show")
+    ->name("genealabs.laravel-governor.api.user-is.show");
