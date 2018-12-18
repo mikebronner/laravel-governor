afba2225c0f9300b17f339ec4ef8d912ea05b8a7
Mike Bronner <mike@genealabs.com>
2018-12-18 11:23:10 -0800

    Remove sourcemaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,5 @@ mix.setPublicPath('dist')
    .js('resources/js/tool.js', 'js')
    .sass('resources/sass/tool.scss', 'css')
    .copy('./node_modules/vue-multiselect/dist/vue-multiselect.min.css', 'css')
-   .sourceMaps()
    .version()
 ;
