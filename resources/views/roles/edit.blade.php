24a49e576d249a83471f778e03d3644c344f0302
Mike Bronner <mike@genealabs.com>
2021-07-19 18:18:49 +0000

    Fixed issue if customAction is null.

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -146,7 +146,7 @@
                                                 scope="col"
                                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                                             >
-                                                {{ ucwords($customAction) }}
+                                                {{ ucwords($customAction ?? "") }}
                                             </th>
                                         @endforeach
 
