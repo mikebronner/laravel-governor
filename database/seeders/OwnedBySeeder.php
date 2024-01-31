db69f32460984dcdfb7d1b06635826ff4a9239e0
Mike Bronner <mike@genealabs.com>
2024-01-31 13:18:03 -0700

    Update OwnedBySeeder.php

--- database/seeders/OwnedBySeeder.php
+++ database/seeders/OwnedBySeeder.php
@@ -7,7 +7,11 @@ namespace GeneaLabs\LaravelGovernor\Database\Seeders;
 use GeneaLabs\LaravelGovernor\Policies\BasePolicy;
 use GeneaLabs\LaravelGovernor\Traits\EntityManagement;
 use GeneaLabs\LaravelGovernor\Traits\GovernorOwnedByField;
+use Illuminate\Container\Container;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 
 class OwnedBySeeder extends Seeder
 {
@@ -16,13 +20,36 @@ class OwnedBySeeder extends Seeder
 
     public function run()
     {
-        $this->getPolicies()
-            ->each(function ($policy) {
-                if (! collect(class_parents($policy))->contains(BasePolicy::class)) {
-                    return collect();
+        $this->getModels()
+            ->each(function ($model) {
+                $this->createGovernorOwnedByFields(new $model);
+            });
+    }
+
+    protected function getModels(): Collection
+    {
+        return collect(File::allFiles(app_path()))
+            ->map(function ($item) {
+                $path = $item->getRelativePathName();
+                $class = sprintf(
+                    '\%s%s',
+                    Container::getInstance()->getNamespace(),
+                    strtr(substr($path, 0, strrpos($path, '.')), '/', '\\'),
+                );
+
+                return $class;
+            })
+            ->filter(function ($class) {
+                $valid = false;
+
+                if (class_exists($class)) {
+                    $reflection = new \ReflectionClass($class);
+                    $valid = $reflection->isSubclassOf(Model::class) &&
+                        !$reflection->isAbstract();
                 }
 
-                $this->createGovernorOwnedByFieldsByPolicy(new $policy);
-            });
+                return $valid;
+            })
+            ->values();
     }
 }
