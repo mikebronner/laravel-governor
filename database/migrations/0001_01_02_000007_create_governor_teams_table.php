6b41994c2c5026e417255c1f1052037d57d80557
Mike Bronner <mike@genealabs.com>
2025-03-15 12:15:50 -0700

    WIP

--- database/migrations/0001_01_02_000007_create_governor_teams_table.php
+++ database/migrations/0001_01_02_000007_create_governor_teams_table.php
@@ -2,22 +2,26 @@
 
 declare(strict_types=1);
 
+use GeneaLabs\LaravelGovernor\Team;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 class CreateGovernorTeamsTable extends Migration
 {
+    protected $userInstance;
+
     public function __construct()
     {
         if (app()->bound("Hyn\Tenancy\Environment")) {
             $this->connection = config("tenancy.db.tenant-connection-name");
         }
+
+        $this->userInstance = app(config('genealabs-laravel-governor.models.auth'));
     }
 
     public function up(): void
     {
         Schema::create('governor_teams', function (Blueprint $table): void {
-            $user = app(config('genealabs-laravel-governor.models.auth'));
             $table->bigIncrements("id");
             $table->unsignedBigInteger("governor_owned_by")
                 ->nullable();
@@ -29,10 +33,18 @@ class CreateGovernorTeamsTable extends Migration
                 ->nullable();
 
             $table->foreign('governor_owned_by')
-                ->references($user->getKeyName())
-                ->on($user->getTable())
+                ->references($this->userInstance->getKeyName())
+                ->on($this->userInstance->getTable())
                 ->onDelete('SET NULL')
                 ->onUpdate('CASCADE');
+        });
+
+        Schema::table($this->userInstance->getTable(), function (Blueprint $table): void {
+            $table->foreignIdFor(Team::class, "current_team_id")
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
         });
     }
 
